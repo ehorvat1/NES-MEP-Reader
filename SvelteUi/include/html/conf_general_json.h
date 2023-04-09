c695f3b0468afe93696189b33aa4286d297de22e
ehorvat1 <108073384+ehorvat1@users.noreply.github.com>
2023-04-09 19:24:03 +0200

    Add files via upload

---
+++ SvelteUi/include/html/conf_general_json.h
@@ -0,0 +1,2 @@
+static const char CONF_GENERAL_JSON[] PROGMEM = R"=="==("g":{"t":"%s","h":"%s","s":%d,"u":"%s","p":"%s"},)=="==";
+const int CONF_GENERAL_JSON_LEN PROGMEM = 49;
---
+++ SvelteUi/include/html/conf_price_json.h
@@ -0,0 +1,2 @@
+static const char CONF_PRICE_JSON[] PROGMEM = R"=="==("p":{"e":%s,"t":"%s","r":"%s","c":"%s","m":%.3f,"f":%s},)=="==";
+const int CONF_PRICE_JSON_LEN PROGMEM = 56;
