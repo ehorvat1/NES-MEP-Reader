56a08be4a5fd3454422b4cc0f9ea2d9a7289b2b4
ehorvat1 <108073384+ehorvat1@users.noreply.github.com>
2023-05-11 18:57:26 +0200

    Add files via upload

--- lib/AmsData/src/AmsData.cpp
+++ lib/AmsData/src/AmsData.cpp
@@ -20,6 +20,20 @@ double NESMEP_ReactiveImportCounter;    //.... this is now NESMEP_data_ready (0/
 double NESMEP_ActiveExportCounter;
 double NESMEP_ReactiveExportCounter;
 double NESMEP_mep_alivecounter;
+
+char NESMEP_Manufacturer[5];
+char NESMEP_Model[9];
+char NESMEP_UtilitySerialNumber[31];
+char NESMEP_FirmwareRevision[8];
+char NESMEP_HardwareRevision[8];
+
+byte NESMEP_MainFirmwareVersionNumber;
+byte NESMEP_FirmwareRevisionNumber;
+byte NESMEP_MainHardwareVersionNumber;
+byte NESMEP_HardwareRevisionNumber;
+String FW_full = "";
+String HW_full = "";
+
 // EHorvat NES-MEP end
 
 void AmsData::apply(AmsData& other) {
@@ -92,7 +106,9 @@ void AmsData::apply(AmsData& other) {
 //        case 2:
             this->listId = other.getListId();
             this->meterId = other.getMeterId();
-            this->meterType = other.getMeterType();
+//            this->meterType = other.getMeterType();//EHorvat NES-MEP
+//            this->meterType_fw = String(NESMEP_MainFirmwareVersionNumber) + "_" + String(NESMEP_FirmwareRevisionNumber);
+            this->meterType_fw = FW_full;//EHorvat NES-MEP
             this->meterModel = other.getMeterModel();
 //            this->reactiveImportPower = other.getReactiveImportPower();
 //            this->reactiveExportPower = other.getReactiveExportPower();
@@ -146,6 +162,18 @@ float AmsData::setfromNESMEP(bool Data_ready, uint16_t ActiveImportPower, long F
     NESMEP_mep_alivecounter = Mep_alivecounter;                 // Send mep_alivecounter instead of ReactiveExportCounter....
 return 123.4;
 }
+
+float AmsData::setfromNESMEP2(char Manufacturer[5], char Model[9], byte MainHardwareVersionNumber, byte HardwareRevisionNumber, byte MainFirmwareVersionNumber, byte FirmwareRevisionNumber, char UtilitySerialNumber[31]) {
+    memcpy(NESMEP_Manufacturer,Manufacturer,5);
+    memcpy(NESMEP_Model,Model,9);
+    memcpy(NESMEP_UtilitySerialNumber,UtilitySerialNumber,31);
+    NESMEP_MainHardwareVersionNumber = MainHardwareVersionNumber;
+    NESMEP_HardwareRevisionNumber= HardwareRevisionNumber;
+    NESMEP_MainFirmwareVersionNumber = MainFirmwareVersionNumber;
+    NESMEP_FirmwareRevisionNumber= FirmwareRevisionNumber;
+return 111.1;
+}
+
 // ************ EHorvat NES-MEP end   ****************
 
 unsigned long AmsData::getLastUpdateMillis() {
@@ -168,16 +196,54 @@ String AmsData::getListId() {
 }
 
 String AmsData::getMeterId() {
-    return this->meterId;
+    return NESMEP_Manufacturer;// EHorvat NES-MEP
 }
 
-uint8_t AmsData::getMeterType() {
-    return this->meterType;
+String AmsData::getMeterType() {  //EHorvat was uint8_t
+    String FW_part1 = "";
+    String FW_part2 = "";
+    FW_part1 = String(NESMEP_MainFirmwareVersionNumber);
+    FW_part2 = String(NESMEP_FirmwareRevisionNumber);
+    FW_full = FW_part1 + "_" + FW_part2;
+//    FW_full = "";
+    return FW_full;// EHorvat NES-MEP
+//    return this->meterType;// EHorvat NES-MEP
+}
+
+String AmsData::getMeterSerial() {  //EHorvat was uint8_t
+    return NESMEP_UtilitySerialNumber;// EHorvat NES-MEP
+}
+String AmsData::getMeterHW() {  //EHorvat new for Hardware Rev
+    uint8_t HW_part1 = 0;
+    uint8_t HW_part2 = 0;
+    uint8_t HW_part3 = 0;
+    uint16_t HW_sum = 0;
+    HW_sum = NESMEP_MainHardwareVersionNumber * 256 + NESMEP_HardwareRevisionNumber;
+    HW_part1 = HW_sum & 31;                         //mask out bits 0..4
+    HW_sum = (HW_sum - HW_part1) / 32;              // shift out bits 0..4
+    HW_part2 = HW_sum & 127;                        //mask out bits 5..11
+    HW_part3 = (HW_sum - HW_part2) / 128;           // shift out bits 5..11
+    HW_full = String(HW_part3) + "." + String(HW_part2) + "." + String(HW_part1);
+    return HW_full;// EHorvat NES-MEP
+}
+String AmsData::getMeterFW() {  //EHorvat new for Firmware Rev
+    uint8_t FW_part1 = 0;
+    uint8_t FW_part2 = 0;
+    uint8_t FW_part3 = 0;
+    uint16_t FW_sum = 0;
+    FW_sum = NESMEP_MainFirmwareVersionNumber * 256 + NESMEP_FirmwareRevisionNumber;
+    FW_part1 = FW_sum & 31;                         //mask out bits 0..4
+    FW_sum = (FW_sum - FW_part1) / 32;              // shift out bits 0..4
+    FW_part2 = FW_sum & 127;                        //mask out bits 5..11
+    FW_part3 = (FW_sum - FW_part2) / 128;           // shift out bits 5..11
+    FW_full = String(FW_part3) + "." + String(FW_part2) + "." + String(FW_part1);
+//    FW_full = "";
+    return FW_full;// EHorvat NES-MEP
 }
 
 String AmsData::getMeterModel() {
 //    return this->meterModel;    // EHorvat NES-MEP
-    return "NES";                 // EHorvat NES-MEP}
+    return NESMEP_Model;                 // EHorvat NES-MEP}
 }
 
 time_t AmsData::getMeterTimestamp() {
