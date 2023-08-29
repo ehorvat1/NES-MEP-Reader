c4df6687d721564b17172e552ed34927196a9d93
ehorvat1 <108073384+ehorvat1@users.noreply.github.com>
2023-08-29 19:23:29 +0200

    Add files via upload

---
+++ lib/SvelteUi/app/src/lib/Last_Counter_Value.svelte
@@ -0,0 +1,41 @@
+<script>
+    export let last_Counter_Info;
+    export let last_Counter_Value;
+    export let last_Counter_Costs;    
+    export let last_Counter_Diff;
+    export let last_expCounter_Info;
+    export let last_expCounter_Value;
+    export let last_expCounter_Costs;    
+    export let last_expCounter_Diff;
+    export let hasExport;
+</script>
+
+<div class="mx-2 text-sm">
+    <strong>Letzte Abrechnung</strong>
+    <br/><br/>
+    <strong>f&uuml;r Bezug</strong>
+    <div class="grid grid-cols-2 mb-3">
+        <div>Info zu Bezug</div>
+        <div class="text-right">{last_Counter_Info}</div>       
+        <div>Z&auml;hlerstand</div>
+        <div class="text-right">{typeof last_Counter_Value !== 'undefined' ? last_Counter_Value.toFixed(0) : '-'} kWh</div>
+        <div>Verbrauch</div>
+        <div class="text-right">{typeof last_Counter_Diff !== 'undefined' ? last_Counter_Diff.toFixed(0) : '-'} kWh</div>
+        <div>Kosten</div>
+        <div class="text-right">{typeof last_Counter_Costs !== 'undefined' ? last_Counter_Costs.toFixed(2) : '-'} EUR</div>
+    </div>
+    {#if hasExport}
+        <strong>f&uuml;r Export</strong>
+        <div class="grid grid-cols-2 mb-3">
+            <div>Info zu Export</div>
+            <div class="text-right">{last_expCounter_Info}</div>       
+            <div>Z&auml;hlerstand</div>
+            <div class="text-right">{typeof last_expCounter_Value !== 'undefined' ? last_expCounter_Value.toFixed(0) : '-'} kWh</div>
+            <div>Export</div>
+            <div class="text-right">{typeof last_expCounter_Diff !== 'undefined' ? last_expCounter_Diff.toFixed(0) : '-'} kWh</div>
+            <div>Einnahmen</div>
+            <div class="text-right">{typeof last_expCounter_Costs !== 'undefined' ? last_expCounter_Costs.toFixed(2) : '-'} EUR</div>
+        </div>
+    {/if}
+</div>
+
---
+++ lib/SvelteUi/app/src/lib/ReactiveData.svelte
@@ -0,0 +1,48 @@
+<script>
+    export let importInstant;
+    export let exportInstant;
+    export let importTotal;
+    export let exportTotal;
+    export let PowerFactorL1; //EHorvat new
+    export let PowerFactorL2; //EHorvat new
+    export let PowerFactorL3; //EHorvat new
+    export let AparentPower; //EHorvat new
+    export let ReactivePower_Q1; //EHorvat new
+    export let ReactivePower_Q2; //EHorvat new
+    export let ReactivePower_Q3; //EHorvat new
+    export let ReactivePower_Q4; //EHorvat new
+</script>
+
+<div class="mx-2 text-sm">
+    <strong>Blind-/Scheinleistung</strong>
+    <div class="grid grid-cols-2 mt-4">
+        <div>Scheinleistung</div>
+        <div class="text-right">{typeof AparentPower !== 'undefined' ? AparentPower.toFixed(0) : '-'} VA</div>
+        <div>BlindL. ind</div>
+        <div class="text-right">{typeof importInstant !== 'undefined' ? importInstant.toFixed(0) : '-'} VAr</div>
+        <div>BlindL. kap</div>
+        <div class="text-right">{typeof exportInstant !== 'undefined' ? exportInstant.toFixed(0) : '-'} VAr</div>
+    </div>
+    <div class="grid grid-cols-2 mt-4">
+        <div>BlindL. Q1</div>
+        <div class="text-right">{typeof ReactivePower_Q1 !== 'undefined' ? ReactivePower_Q1.toFixed(0) : '-'} VAr</div>        
+        <div>BlindL. Q2</div>
+        <div class="text-right">{typeof ReactivePower_Q2 !== 'undefined' ? ReactivePower_Q2.toFixed(0) : '-'} VAr</div>        
+        <div>BlindL. Q3</div>
+        <div class="text-right">{typeof ReactivePower_Q3 !== 'undefined' ? ReactivePower_Q3.toFixed(0) : '-'} VAr</div>        
+        <div>BlindL. Q4</div>
+        <div class="text-right">{typeof ReactivePower_Q4 !== 'undefined' ? ReactivePower_Q4.toFixed(0) : '-'} VAr</div>        
+        <div>Ges.BL ind</div>
+        <div class="text-right">{typeof importTotal !== 'undefined' ? importTotal.toFixed(1) : '-'} kVArh</div>
+        <div>Ges.BL kap</div>
+        <div class="text-right">{typeof exportTotal !== 'undefined' ? exportTotal.toFixed(1) : '-'} kVArh</div>
+    </div>    
+    <div class="grid grid-cols-2 mt-4">
+        <div>Cos Phi L1</div>
+        <div class="text-right">{typeof PowerFactorL1 !== 'undefined' ? PowerFactorL1.toFixed(2) : '-'} W/VA</div>        
+        <div>Cos Phi L2</div>
+        <div class="text-right">{typeof PowerFactorL2 !== 'undefined' ? PowerFactorL2.toFixed(2) : '-'} W/VA</div>        
+        <div>Cos Phi L3</div>
+        <div class="text-right">{typeof PowerFactorL3 !== 'undefined' ? PowerFactorL3.toFixed(2) : '-'} W/VA</div>
+    </div>
+</div>
