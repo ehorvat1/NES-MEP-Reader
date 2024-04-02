f6730eee7e13e29b2d16c137c19d21f9437fb98d
ehorvat1 <108073384+ehorvat1@users.noreply.github.com>
2024-04-02 12:13:28 +0200

    Updated build date in Status page

--- lib/SvelteUi/app/src/lib/StatusPage.svelte
+++ lib/SvelteUi/app/src/lib/StatusPage.svelte
@@ -158,7 +158,7 @@
             Installierte Version: pre2.7-ge
         </div>        
         <div class="my-2"><!-- EHorvat new info-->
-            Datum: 11. Dez. 2023
+            Datum: 2. April 2024
         </div>
         {#if sysinfo.security == 0 || data.a}
         <div class="my-2 flex">
